12b5c03633dafc1756d6c4ee502a0d08e75350bc
LPX <yoon.hansup1@gmail.com>
2023-07-09 21:59:10 +0000

    update(deps)

--- packages/evmos-wallet/src/internal/wallet/functionality/walletconnect/walletconnectConstants.ts
+++ packages/evmos-wallet/src/internal/wallet/functionality/walletconnect/walletconnectConstants.ts
@@ -2,9 +2,9 @@
 // SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/apps/blob/main/LICENSE)
 
 import {
-  EthereumClient,
-  modalConnectors,
-  walletConnectProvider,
+  EthereumClient, 
+  w3mConnectors, 
+  w3mProvider
 } from "@web3modal/ethereum";
 import { configureChains, createClient } from "wagmi";
 import { evmos } from "wagmi/chains";
@@ -13,13 +13,11 @@ export const projectId = "ae920fe62c5a565cfaaa6edacbbb6fa7";
 
 const chains = [evmos];
 // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId }),
-]);
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
 
 export const wagmiClient = createClient({
   autoConnect: false,
-  connectors: modalConnectors({ appName: "web3Modal", chains }),
+  connectors: w3mConnectors({ projectId, chains }),
   provider,
 });
 
